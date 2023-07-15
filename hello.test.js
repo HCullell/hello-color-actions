7b6641ef0dbe9d925ce3fff8f1c1550c7d6b7c45
HCullell <henrycullell@yahoo.com>
2023-07-15 11:35:55 -0400

    job desde sudamerica

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola a todos desde Chile");
+    expect(hello.hello()).toEqual("Hola desde sudamerica");
   });
 });
